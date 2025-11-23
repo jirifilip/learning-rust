5f07da69dc5cdc5cdf05df84dce3353f02ba376c
Jiří Filip <j.f.ilip@seznam.cz>
2025-11-23 15:48:35 +0100

    Add simple file with unit test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+fn add(a: i32, b: i32) -> i32 {
+    return a + b;
+}
+
+
 fn main() {
     println!("Hello, world!");
+    println!("Add result: {}", add(2, 3));
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(2, 1), 3);
+    }
 }
