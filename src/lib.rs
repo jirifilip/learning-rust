1cd4a993be5e2ed11dc9898fe9058feea31a5d66
Jiří Filip <j.f.ilip@seznam.cz>
2025-11-23 16:00:11 +0100

    Add doc test

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+/// This function is doing some adding.
+///
+/// Next line that should maybe be here?
+///
+/// ```
+/// let result = learning_rust::add(1, 2);
+/// assert_eq!(result, 3);
+/// ```
+pub fn add(a: i32, b: i32) -> i32 {
+    return a + b;
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(2, 1), 3);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,7 @@
-fn add(a: i32, b: i32) -> i32 {
-    return a + b;
-}
+use learning_rust::add;
 
 
 fn main() {
     println!("Hello, world!");
     println!("Add result: {}", add(2, 3));
-}
-
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_add() {
-        assert_eq!(add(2, 1), 3);
-    }
 }
